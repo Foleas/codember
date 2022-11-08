f9fa3d0627845b92abb6f498eb54a1d8b0ffee58
Foleas <sebastianfilipetto@gmail.com>
2022-11-08 18:17:27 -0300

    CheckUser

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const fetchUsers = async () => {
       const linesArray = text
         .split("\n\n")
         .map((line) => line.split(/ |\n/))
-        // .filter((_item, index) => index < 2)
+        // .filter((_item, index) => index < 10)
         .map((user) => {
           const userObject = {};
           user.forEach((attr) => {
@@ -24,10 +24,25 @@ const fetchUsers = async () => {
     });
 };
 
+const checkUsersProps = (user) => {
+  for (let i = 0; i < userKeys.length; i++) {
+    if (!user.hasOwnProperty(userKeys[i]) || userKeys[i] === "") {
+      return false;
+    }
+  }
+
+  return true;
+};
+
 async function main() {
   const users = await fetchUsers();
-  console.log("users", users);
-  //   console.log(usersEndPointTest);
+  //   console.log("USER RAW", users.length);
+  const usersFiltered = users.filter((user) => checkUsersProps(user));
+  console.log(
+    "USERS FILTERED",
+    usersFiltered[usersFiltered.length - 1],
+    usersFiltered.length
+  );
 }
 
 main();
